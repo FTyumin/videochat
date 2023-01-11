8debbcc16deac1f854890392678fa62deb21a795
FTyumin <feodor.tjumin28@gmail.com>
2023-01-11 18:39:35 +0200

    Created an admin user and serializers.py

--- project/models.py
+++ project/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    room_title = models.CharField(max_length=25)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.room_title
