4fcb065d21671a980cfdcab15a5023d507a2601a
FTyumin <feodor.tjumin28@gmail.com>
2023-01-15 14:33:11 +0200

    Editing the frontend

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,8 @@
-
 function App() {
   
-
+  
   return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-12 text-center">
-            <img
-              src= "img/agora_logo.png"
-              alt="Agora Logo"
-              className="block img-fluid"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="container my-5">
-        <div className="row">
-          <div className="col">
-            <div className="btn-group">
-
-            </div>
-
-          </div>
-
-        </div>
-        
-      </div>
-    </div>
-        
-  )
+    
+            
+  );
 }
-
-export default App
