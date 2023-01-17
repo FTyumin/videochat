f965ba27f7acd13cd41d81b16e4c83c3f5255565
FTyumin <feodor.tjumin28@gmail.com>
2023-01-17 21:56:22 +0200

    Some small changes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,27 +4,25 @@ function App() {
   return (
     <main id="app">
       <main>
-        <div class="container">
-          <div class="row">
+        <div className="container">
+          <div className="row">
           <div class="col-12 text-center">
               <img
                 src="src/img/agora_logo.png"
                 alt="Agora Logo"
-                class="block img-fluid"
+                className="block img-fluid"
               />
           </div>
           </div>
         </div>
-        <div class="container my-5">
-          <div class="row">
-            <div class="col">
-              <div class="btn-group" role="group">
-                {/*% for singleUser in allUsers%*/}
+        <div className="container my-5">
+          <div className="row">
+            <div className="col">
+              <div className="btn-group" role="group">
+              
                 <button
                   type="button"
                   class="btn btn-primary mr-2"
-
-                  //@click="placeCall('{{singleUser.id}}','{{singleUser}}')"
                 ></button>
               </div>  
             </div>
