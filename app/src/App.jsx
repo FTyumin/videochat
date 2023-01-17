6ad9aae7460b4400aeccf40216dd65f93286ddc2
FTyumin <feodor.tjumin28@gmail.com>
2023-01-17 21:14:06 +0200

    Some edits

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,8 +5,32 @@ function App() {
     <main id="app">
       <main>
         <div class="container">
-          <div class="row"></div>
+          <div class="row">
+          <div class="col-12 text-center">
+              <img
+                src="src/img/agora_logo.png"
+                alt="Agora Logo"
+                class="block img-fluid"
+              />
           </div>
+          </div>
+        </div>
+        <div class="container my-5">
+          <div class="row">
+            <div class="col">
+              <div class="btn-group" role="group">
+                {/*% for singleUser in allUsers%*/}
+                <button
+                  type="button"
+                  class="btn btn-primary mr-2"
+
+                  //@click="placeCall('{{singleUser.id}}','{{singleUser}}')"
+                ></button>
+              </div>  
+            </div>
+          </div>
+        </div>
+
 
     </main>
 
@@ -14,3 +38,5 @@ function App() {
             
   );
 }
+
+export default App
