112cc30ee17ce04570ea6a42e4ccfcf0112712e4
FTyumin <feodor.tjumin28@gmail.com>
2023-01-17 20:31:50 +0200

    Changed the frontend, again

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,15 @@ function App() {
   
   
   return (
-    
+    <main id="app">
+      <main>
+        <div class="container">
+          <div class="row"></div>
+          </div>
+
+    </main>
+
+    </main>
             
   );
 }
