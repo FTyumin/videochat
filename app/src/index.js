5f1f61d521f53571dd8c4dc5c315026d53038b09
FTyumin <feodor.tjumin28@gmail.com>
2023-01-18 18:38:41 +0200

    Edits

--- app/src/index.js
+++ app/src/index.js
@@ -10,4 +10,28 @@ const app = {
     incomingCaller: "",
     agoraChannel: null
 };
-  
+
+function initUserOnlineChannel() {
+    const userOnlineChannel = pusher.subscribe("presence-online-channel")
+
+    userOnlineChannel.bind("pusher:subscription_succeded", (data) =>{
+
+        let members = Object.keys(data.members).map((k) => data.members[k])
+        this.onlineUsers = members
+    })
+
+    userOnlineChannel.bind("pusher:member_added", (data) => {
+        let user = data.info;
+        // check user availability
+        const joiningUserIndex = this.onlineUsers.findIndex(
+            (data) => data.id === user.id
+        )
+        if (joiningUserIndex < 0) {
+            this.onlineUsers.push(user);
+          }
+        
+    })}
+        
+    
+
+    
