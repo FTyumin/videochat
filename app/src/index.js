7c2f6d172f722629d3db7f3011276018ec5abbe6
FTyumin <feodor.tjumin28@gmail.com>
2023-01-18 18:18:32 +0200

    Added a new js file

---
+++ app/src/index.js
@@ -0,0 +1,13 @@
+const app = {
+    callPlaced: false,
+    client: null,
+    localStream: null,
+    mutedAudio: false,
+    mutedVideo: false,
+    userOnlineChannel: null,
+    onlineUsers: [],
+    incomingCall: false,
+    incomingCaller: "",
+    agoraChannel: null
+};
+  
