9a8a3feb62fe3281ac1caa9a39aa2d1024c943ef
FTyumin <feodor.tjumin28@gmail.com>
2023-01-22 15:32:22 +0200

    Changes to index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,5 @@
 const app = {
+  data:{
     callPlaced: false,
     client: null,
     localStream: null,
@@ -9,44 +10,33 @@ const app = {
     incomingCall: false,
     incomingCaller: "",
     agoraChannel: null
-};
+  },
+    
 
-function initUserOnlineChannel() {
-    const userOnlineChannel = pusher.subscribe("presence-online-channel")
 
-    userOnlineChannel.bind("pusher:subscription_succeded", (data) =>{
+mounted() {
+  this.initUserOnlineChannel()
+},
 
+methods:
+
+  function initUserOnlineChannel() {
+      const userOnlineChannel = pusher.subscribe("presence-online-channel")
+
+      userOnlineChannel.bind("pusher:subscription_succeded", (data) => {
         let members = Object.keys(data.members).map((k) => data.members[k])
         this.onlineUsers = members
-    })
+      })
+      
+  }
 
-    userOnlineChannel.bind("pusher:member_added", (data) => {
-        let user = data.info;
-        // check user availability
-        const joiningUserIndex = this.onlineUsers.findIndex(
-            (data) => data.id === user.id
-        )
-        if (joiningUserIndex < 0) {
-            this.onlineUsers.push(user);
-          }
-        
-    })}
+    // Start Pusher Presence Channel Event Listeners
 
-    userOnlineChannel.bind("pusher:member_removed", (data) => {
-        let user = data.info;
-        const leavingUserIndex = this.onlineUsers.findIndex(
-          (data) => data.id === user.id
-        );
-        this.onlineUsers.splice(leavingUserIndex, 1);
-      });
+    
 
-      userOnlineChannel.bind("pusher:subscription_error", (err) => {
-        console.log("Subscription Error", err);
-      });
 
-      userOnlineChannel.bind("an_event", (data) => {
-        console.log("a_channel: ", data);
-      });
+    
+}
         
     
 
